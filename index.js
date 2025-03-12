d98e3c3f069b43f6a2396b69890ae4301beacf95
dylanfox44 <131827295+dylanfox44@users.noreply.github.com>
2025-03-12 22:45:10 +0000

    test

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js"
 
-const supabaseUrl = 'https://puckhulgftuosunkeunu.supabase.co'
+const supabaseUrl = 'https://eunwothedikttpuoslnr.supabase.co'
 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1Y2todWxnZnR1b3N1bmtldW51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2NTYxMTQsImV4cCI6MjA1NzIzMjExNH0.ekV65ofIzo7syfxqYCrHv-QgA3nhzFUBoI3AEYV_xzI'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1bndvdGhlZGlrdHRwdW9zbG5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE1NzQyOTQsImV4cCI6MjA1NzE1MDI5NH0.-J9GgbAFhSlN2be-lakGlRM9w3P9j2q9g1aYBZJ61X8'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function getBooks() {
-  let { data: favbooks, error } = await supabase
-    .from('favbooks')
+  let { data: books, error } = await supabase
+    .from('books')
     .select('*')
-  let bookList = document.getElementById('favbooks');
+  let bookList = document.getElementById('books');
   
-  for (let book of favbooks) {
-     bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.isbn}</td></tr>`;
+  for (let book of books) {
+     bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.created_at}</td></tr>`;
    }
 }
 
